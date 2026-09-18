143e740198be2127ce88b09cd37a31809c7b3f98
Asier Aguado <asieraguadoserrano@gmail.com>
2026-09-18 19:00:42 +0200

    codigo e input actions
    añadido de código e input actions sencillos

--- AguadoAsier_ZAXXON/Assets/_MyAssets/01_Scripts/PlayerManager.cs
+++ AguadoAsier_ZAXXON/Assets/_MyAssets/01_Scripts/PlayerManager.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public float moveSpeed;
+    [SerializeField] float desplSpeed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveSpeed = 36f;
+        desplSpeed = 6f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector3.right * desplSpeed * Time.deltaTime);
     }
 }
