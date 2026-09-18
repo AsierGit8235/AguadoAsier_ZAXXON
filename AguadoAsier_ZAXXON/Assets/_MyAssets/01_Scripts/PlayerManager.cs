93897c0faa8d6a80c39d238dd03084289da1709e
Asier Aguado <asieraguadoserrano@gmail.com>
2026-09-18 19:27:43 +0200

    añadido código
    añadido del move x de la nave e input actions nuevos (disparar)

--- AguadoAsier_ZAXXON/Assets/_MyAssets/01_Scripts/PlayerManager.cs
+++ AguadoAsier_ZAXXON/Assets/_MyAssets/01_Scripts/PlayerManager.cs
@@ -5,6 +5,37 @@ public class PlayerManager : MonoBehaviour
     public float moveSpeed;
     [SerializeField] float desplSpeed;
 
+    //Input System
+    MyInputActions inputActions;
+
+    //movimiento en x
+    float MoveX;
+
+    private void Awake()
+    {
+        inputActions = new MyInputActions();
+
+        inputActions.Player.Fire.started += _ => Shoot();
+
+        inputActions.Player.MoveX.performed += ctx => MoveX = ctx.ReadValue<float>();
+        inputActions.Player.MoveX.canceled += ctx => MoveX = 0f; 
+    }
+
+    void Shoot()
+    {
+        print("PUUUUUM");
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputActions.Disable();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
